3fb818a8cdd3cbfe2077b4a839b03d9cc9472a65
allen99 <kn99.allen@gmail.com>
2022-05-29 22:53:01 +1000

    nice tag view

--- src/components/TaskInfo.jsx
+++ src/components/TaskInfo.jsx
@@ -7,11 +7,16 @@ const TaskInfo = ({task, delete_todo, edit_todo, done_todo}) => {
     return (
         <Accordion.Body>
             <div className="card card-body">
+                {task.tag_name &&
+                <Button variant="outline-dark" size="" disabled>
+                    {task.tag_name}
+                </Button>}
+                <p>  </p>
                 {task.date_begin && <p>Дата начала: {task.date_begin} </p>}
                 {task.date_end && <p>Дата окончания: {task.date_end}</p>}
                 {task.place && <p>Место: {task.place}</p>}
                 {task.text && <p>Описание: {task.text}</p>}
-                {task.tag_name && <p>Тег: {task.tag_name}</p>}
+
                 <ButtonGroup aria-label="Basic example">
                     <Button variant="outline-secondary" onClick={() => edit_todo(task)}>Изменить</Button>
                     <Button variant="outline-danger" onClick={() => delete_todo(task._id)}>Удалить</Button>
